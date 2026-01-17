0d65e277d4ddb5eb1229765e9d44ac958bae9035
burhan11231 <burhan.ah.shkh@gmail.com>
2026-01-17 12:42:37 +0530

    Update firebase-rtdb.ts

--- src/lib/firebase-rtdb.ts
+++ src/lib/firebase-rtdb.ts
@@ -1,4 +1,8 @@
-import { getDatabase } from 'firebase/database';
-import app from './firebase';
+// lib/firebase-rtdb.ts
+import { getDatabase } from 'firebase/database'
+import app from './firebase'
 
-export const rtdb = getDatabase(app);
+export const rtdb = getDatabase(
+  app,
+  process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
+)
