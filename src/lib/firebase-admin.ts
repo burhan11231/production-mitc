5f09d355906beff752fb2c7a65b0cc19304d37c7
burhan11231 <burhan.ah.shkh@gmail.com>
2026-01-17 22:39:46 +0530

    Update firebase-admin.ts

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,10 +1,12 @@
 import admin from 'firebase-admin';
 
-const hasApp = admin.apps.length > 0;
-
-if (!hasApp) {
+if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID!,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+    }),
   });
 }
 
