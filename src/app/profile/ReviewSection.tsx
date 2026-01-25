1832e3a48de70f1191d7ad33879132182b9a5132
burhan11231 <burhan.ah.shkh@gmail.com>
2026-01-26 01:52:30 +0530

    Update ReviewSection.tsx

--- src/app/profile/ReviewSection.tsx
+++ src/app/profile/ReviewSection.tsx
@@ -71,7 +71,7 @@ export default function ReviewSection() {
   /* ---------------- UI ---------------- */
 
   return (
-    
+    <div className="p-6">
       {loading && (
         <div className="h-24 bg-gray-100 animate-pulse rounded-xl" />
       )}
