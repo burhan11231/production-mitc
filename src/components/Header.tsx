9914b29b1278834e552b65efdcebe97ed6004019
burhan11231 <burhan.ah.shkh@gmail.com>
2026-01-30 09:11:06 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -94,7 +94,7 @@ export default function Header() {
   unoptimized
 />
 
-  <div className="flex flex-col justify-center leading-none">
+  <div className="flex flex-col justify-center leading-none [line-height:1]">
     <div className="text-sm lg:text-xl font-bold tracking-tight text-gray-900">
       {settings?.businessName || 'MITC'}
     </div>
